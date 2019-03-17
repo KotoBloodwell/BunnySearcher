2de40deb483c753600831f240f3493905268c025
Juliana Loaiza Labrador <juliana.loaiza@globant.com>
2019-03-17 13:17:15 -0500

    Initial Commit

---
+++ ImageSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageSearcher
+//
+//  Created by Juliana Loaiza Labrador on 3/17/19.
+//  Copyright © 2019 Juliana Loaiza Labrador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
